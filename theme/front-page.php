b9329c4ff831a4a41eb3f9e82160b113d5394cd9
FedeM <mariottifederico@gmail.com>
2026-01-16 00:45:01 -0300

    Sección de paneles

--- theme/front-page.php
+++ theme/front-page.php
@@ -46,7 +46,21 @@ get_template_part('template-parts/layout/header', 'theme');
             </div>
         </div>
     </section>
-    
+
+    <!-- PANEL SÁNDWICH -->
+    <section class="py-24 bg-[var(--color-gray)]">
+        <div class="container mx-auto max-w-[1300px]">
+            <!-- Título -->
+            <div class="text-center mb-24">
+                <h2 class="text-[var(--color-primary)] text-3xl md:text-4xl font-semibold mb-2"><span class="text-[var(--color-secondary)] relative inline-block">PANEL SÁNDWICH</span> para tu proyecto</h2>
+            </div>
+
+            <!-- Paneles -->
+            <div>
+                <?php echo do_shortcode('[pt_paneles]'); ?>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
