6b1911c4ed73b0fefaf58f0051603aea4dd6d3cc
FedeM <mariottifederico@gmail.com>
2026-01-16 19:31:55 -0300

    Sección de contacto

--- theme/front-page.php
+++ theme/front-page.php
@@ -61,6 +61,32 @@ get_template_part('template-parts/layout/header', 'theme');
             </div>
         </div>
     </section>
+
+    <!-- Pide Presupuesto -->
+    <section class="pt-24 bg-[var(--color-gray-dark)]">
+        <div class="container mx-auto max-w-[1300px]">
+            <!-- Título -->
+            <div class="text-center mb-24">
+                <h2 class="text-[var(--color-primary)] text-3xl md:text-4xl font-semibold mb-2">Pide <span class="text-[var(--color-secondary)] relative inline-block">PRESUPUESTO
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/vector_02.png" alt="" class="absolute -bottom-7 w-[270px] h-[25px]">
+                </span></h2>
+            </div>
+
+            <!-- Formulario -->
+            <?php echo do_shortcode('[contact-form-7 id="4f15697" title="Formulario de contacto 1"]'); ?>
+        </div>
+    </section>
+
+    <!-- Profesionales y cercanos -->
+    <section class="py-24 bg-white">
+        <div class="container mx-auto max-w-[1300px]">
+            <!-- Título -->
+            <div class="text-center mb-24">
+                <h2 class="text-[var(--color-primary)] text-3xl md:text-4xl font-semibold mb-2">Profesionales y <span class="text-[var(--color-secondary)] relative inline-block">CERCANOS</span></h2>
+            </div>
+
+            
+        </div>
 </main>
 
 <?php
