ce87413aeea6a505db1eb1a110a7b4244a179455
FedeM <mariottifederico@gmail.com>
2026-01-16 19:30:24 -0300

    Clases "seguras" para que las detecte CF7

---
+++ theme/tailwind-safelist.php
@@ -0,0 +1,50 @@
+<?php
+// Tailwind safelist for CF7 – do not remove
+?>
+
+<div class="
+grid grid-cols-1 md:grid-cols-3 gap-4 gap-x-6 gap-y-6
+w-full
+w-5
+h-5
+rounded-full 
+rounded-xl
+rounded-lg
+border border-[(--color-gray)] border-gay
+px-4 py-3
+text-sm
+bg-white
+text-[(--color-gray-dark)]
+text-[#4F5665] 
+font-semibold
+focus:outline-none
+focus:ring-2
+focus:ring-primary
+bg-[(--color-primary)]
+bg-[(--color-primary-dark)]
+text-white
+hover:bg-[(--color-primary-dark)]
+transition
+accent-secondary
+font-regular
+mt-6
+mt-12
+space-y-3
+space-y-4 
+mb-2
+mr-5
+relative
+mt-16
+text-center
+z-10
+translate-y-1/2
+w-2xs
+sm:w-2xs
+sm:px-6 lg:px-0 
+max-w-6xl mx-auto
+justify-center
+container
+flex
+block
+bg-primary
+"></div>
