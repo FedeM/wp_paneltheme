260c01a458a4b3da97156719a16fe1afe4ffceea
FedeM <mariottifederico@gmail.com>
2026-01-17 02:34:38 -0300

    Sección Asesoramiento

--- theme/front-page.php
+++ theme/front-page.php
@@ -178,7 +178,7 @@ get_template_part('template-parts/layout/header', 'theme');
     </section>
 
     <!-- Reseñas de clientes -->
-    <section class="py-24 bg-[var(--color-gray)]">
+    <section class="py-18 bg-[var(--color-gray)]">
         <div class="container mx-auto max-w-[1300px]">
 
             <!-- Contenedor principal: organiza título y flechas -->
@@ -245,6 +245,31 @@ get_template_part('template-parts/layout/header', 'theme');
                 </div>
             </div>
 
+        </div>
+    </section>
+
+    <!-- Necesitas asesoramiento -->
+    <section class="py-15 bg-[var(--color-secondary)]">
+        <div class="container mx-auto max-w-[1300px]">
+            <div class="relative text-center mx-6 md:mx-0">
+                <h2 class="text-[var(--color-primary)] text-3xl md:text-4xl font-semibold mb-8">¿Necesitas asesoramiento <br><span class="text-white">PERSONALIZADO</span>?</h2>
+            
+                <p>Todos nuestros <span class="font-bold">Paneles Sandwich</span> disponen de guías de montaje exclusivas.</p>
+                <p class="mt-2">Además, disponemos de un <span class="font-bold">departamento técnico</span> para resolver sus dudas</p>
+                <p class="mt-2">y calculamos los elementos necesarios para su correcto montaje e instalación.</p>
+
+                <a href="#" class="inline-block px-8 py-3 mt-10 bg-white text-[var(--color-primary)] border-2 border-[var(--color-secondary)] font-bold rounded-full hover:bg-gray-50 transition">
+                    CONTACTA CON NOSOTROS
+                </a>
+            
+                <!-- Flechita blanca -->
+                <div class="hidden lg:block absolute top-6 left-3 xl:left-36">
+                    <img 
+                    src="<?php echo get_template_directory_uri(); ?>/images/vector_asesoramiento.png" 
+                    alt="Flecha blanca decorativa"
+                    />
+                </div>
+            </div>
         </div>
     </section>
 </main>
